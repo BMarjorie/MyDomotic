e86ceab9927c1a429cf62080790ee7859b8634c5
David Gaschard <gaschard.david@gmail.com>
2015-12-12 19:54:47 +0100

    Switch Ampoule

--- app/src/main/java/com/example/utilisateur/mydomotic/LED_activity.java
+++ app/src/main/java/com/example/utilisateur/mydomotic/LED_activity.java
@@ -4,30 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 
 public class LED_activity extends AppCompatActivity {
 
-    @Override
+    private Switch mySwitch;
+    private ImageView lampe;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_led_activity);
-        ImageView lampe_on = (ImageView) findViewById(R.id.ampoule);
-        lampe_on.setImageResource(R.drawable.lampe);}
-      //  ImageView lampe_off = (ImageView) findViewById(R.id.ampoule);
-
-    //public void onClick(View view) {
-    //    switch (view.getId()){
-  // if (sw==true)
-  // { }
-    
-
+        lampe = (ImageView) findViewById(R.id.ampoule);
+        mySwitch = (Switch) findViewById(R.id.switch1);
+        lampe.setImageResource(R.drawable.lampe_off);
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    lampe.setImageResource(R.drawable.lampe_on);
+                } else {
+                    lampe.setImageResource(R.drawable.lampe_off);
+                }
+            }
+        });
+    }
     public void return_t(View view){
         Intent intent1 = new Intent(this, MainActivity.class);
         intent1.putExtra("connection", true);
         startActivity(intent1);
     }
-
-
-
 }
